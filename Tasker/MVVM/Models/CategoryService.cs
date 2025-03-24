01786b946883b67c465d7151f85f7667176e1286
viva <emmulatordon@gmail.com>
2025-03-24 06:02:18 +0100

    started work on the categories section

---
+++ Tasker/MVVM/Models/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasker.MVVM.Models
+{
+   public class CategoryService
+    {
+        public ObservableCollection<Category> GetAllCategories()
+        {
+            return new ObservableCollection<Category>()
+            {
+                new Category(){Id=1,CategoryName=".Net MAUI Course",Color="#cf14df"},
+                new Category(){Id=2,CategoryName="Tutorials",Color="#df6f14"},
+                new Category(){Id=3,CategoryName="Shopping",Color="#14df80"}
+
+            };
+        }
+    }
+}
--- Tasker/MVVM/ViewModels/MainViewModel.cs
+++ Tasker/MVVM/ViewModels/MainViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tasker.MVVM.Models;
 
 namespace Tasker.MVVM.ViewModels
 {
     public class MainViewModel
     {
+        public ObservableCollection<Category> Categories { get; set; }
+        public MainViewModel()
+        {
+            Categories = FillData();
+        }
+
+        private ObservableCollection<Category> FillData()
+        {
+            return new CategoryService().GetAllCategories();
+        }
     }
 }
