967543d3a40c6cc421d28af2baa9025913bf24be
viva <emmulatordon@gmail.com>
2025-03-26 07:24:49 +0100

    create category section 3

--- Tasker/MVVM/Models/MyTaskService.cs
+++ Tasker/MVVM/Models/MyTaskService.cs
@@ -15,11 +15,11 @@ namespace Tasker.MVVM.Models
             {
                 new MyTask(){TaskName="Upload exercise files",Completed=false,CategoryId=1},
                 new MyTask(){TaskName="Plan next course",Completed=false,CategoryId=1},
-                new MyTask(){TaskName="Plan next course",Completed=false,CategoryId=1},
+                new MyTask(){TaskName="Plan next course",Completed=true,CategoryId=1},
                 new MyTask(){TaskName="Upload new Asp.net course on youtube",Completed=false,CategoryId=2},
                 new MyTask(){TaskName="Fix settings.cs file of the project",Completed=false,CategoryId=2},
                 new MyTask(){TaskName="Update github repository",Completed=false,CategoryId=2},
-                new MyTask(){TaskName="Find a laptop bag",Completed=false,CategoryId=3},
+                new MyTask(){TaskName="Find a laptop bag",Completed=true,CategoryId=3},
                 new MyTask(){TaskName="Finish two beds",Completed=false,CategoryId=4},
             }; 
         }
